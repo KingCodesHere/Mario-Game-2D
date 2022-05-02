b5e824710b806a66ddb9ead32e458481c374da7b
mwan0040 <mwan0040@student.monash.edu>
2022-05-02 23:23:02 +1000

    JavaDoc for these classes are uploaded for submission - Enemy : Koopa, Goomba - Toad - Status

--- src/game/roles/Goomba.java
+++ src/game/roles/Goomba.java
@@ -17,17 +17,34 @@ import java.util.HashMap;
 import java.util.Map;
 /**
  * A little fungus guy.
+
+ * @author Kenda Wan
+ * @version 1.0.0
+ *
  */
 public class Goomba extends Enemy {
+
+	/**
+	 * List of behaviours in hashmap, organising the priority level
+	 */
 	private final Map<Integer, Behaviour> behaviours = new HashMap<>(); // priority, behaviour
 
 	/**
-	 * Constructor.
+	 * Constructor
+	 * returning super class: Enemy
 	 */
 	public Goomba() {
 		super("Goomba", 'g', 20,10,"kicks");
 
 	}
+	/**
+	 * This playTurn override the parent class
+	 * @param actions    collection of possible Actions for this Actor
+	 * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+	 * @param map        the map containing the Actor
+	 * @param display    the I/O object to which messages may be written
+	 * @return the parent class: Enemy playTurn
+	 */
 
 	@Override
 	public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
