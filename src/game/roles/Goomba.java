0b182d984d06fdd86b6d0f145f8fe775c1ae6e91
mwan0040 <mwan0040@student.monash.edu>
2022-05-02 18:20:30 +1000

    Enemy Class, Koopa,Goomba modified: - added condition loop - removed unnecessary conditions

--- src/game/roles/Goomba.java
+++ src/game/roles/Goomba.java
@@ -7,6 +7,7 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import game.RandomRange;
 import game.action.AttackAction;
 import game.behaviours.Behaviour;
@@ -36,26 +37,18 @@ public class Goomba extends Enemy {
 		return super.playTurn(actions, lastAction, map, display);
 	}
 
-	/**
-	 * At the moment, we only make it can be attacked by Player.
-	 * You can do something else with this method.
-	 * @param otherActor the Actor that might perform an action.
-	 * @param direction  String representing the direction of the other Actor
-	 * @param map        current GameMap
-	 * @return list of actions
-	 * @see Status#HOSTILE_TO_ENEMY
-	 */
+
 
 	@Override
 	public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
 		ActionList actions = new ActionList();
-		// it can be attacked only by the HOSTILE opponent, and this action will not attack the HOSTILE enemy back.
+
 		if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
 			actions.add(new AttackAction(this,direction));
 		}
+
 		return actions;
 	}
-
 
 
 
